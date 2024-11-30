46e7a0bb815a0f606027f2343758ab2661c3b964
Eyad Ahmed <dev.esnosy@gmail.com>
2024-11-30 21:16:06 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Thanks to [UAD-NG](https://github.com/Universal-Debloater-Alliance/universal-android-debloater-next-generation) for `uad_lists.txt`
---
+++ eyad_universal_android_debloater.py
@@ -0,0 +1,41 @@
+import json
+import subprocess as sp
+from typing import Dict, List
+
+from clize import run
+
+
+def disable_package(pkg_name: str) -> None:
+    sp.run(f"adb shell pm disable-user --user 0 {pkg_name}".split())
+
+
+def get_enabled_package_names() -> List[str]:
+    return [
+        s[len("package:") :]
+        for s in sp.run(
+            "adb shell pm list packages --user 0 -e".split(),
+            capture_output=True,
+            check=True,
+        )
+        .stdout.decode()
+        .splitlines()
+    ]
+
+
+def disable_all_recommended_software():
+    with open("uad_lists.json", "r") as file:
+        uad_lists: Dict[str, Dict[str, str]] = json.load(file)
+    for pkg_name in get_enabled_package_names():
+        uad_info = uad_lists.get(pkg_name)
+        if uad_info is None:
+            continue
+        if uad_info["removal"] == "Recommended":
+            disable_package(pkg_name)
+
+
+def disable_play_store():
+    disable_package("com.android.vending")
+
+
+if __name__ == "__main__":
+    run(disable_all_recommended_software, disable_play_store)
